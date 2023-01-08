e013e70775ac57454101afc84ccf968870987cc2
YunNamwon <109219153+YunNamwon@users.noreply.github.com>
2023-01-08 18:50:49 +0900

    v2

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function(){
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+/* scroll 과부화 방지 _.throttle(함수, 시간)*/
+window.addEventListener('scroll', _.throttle(function(){
+  console.log(window.scrollY);
+  if(window.scrollY > 500){
+    //배지 숨기기 gsap.to(요소, 지속시간, 옵션) / badgeEl.style.display = 'none';
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+    //badgeEl.style.display = 'none';
+  } else {
+    //배지 보여주기 / badgeEl.style.display = 'block';
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
